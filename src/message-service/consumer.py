3f65e8643f471c437dbc07ed487d7e543c1e1168
Mudassir Ali <163848374+iammudassirali@users.noreply.github.com>
2025-01-13 10:19:59 +0500

    Update consumer.py

--- src/message-service/consumer.py
+++ src/message-service/consumer.py
@@ -1,1 +1,16 @@
+import pika
 
+def callback(ch, method, properties, body):
+    print(f" [x] Received {body}")
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='chat_queue')
+
+channel.basic_consume(queue='chat_queue',
+                      on_message_callback=callback,
+                      auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
